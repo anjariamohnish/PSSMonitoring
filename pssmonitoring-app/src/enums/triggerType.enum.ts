893dc0b9dbe976e7a6d2ca6b3b4058f406c3ca41
anjariamohnish <anjariamohnish@gmail.com>
2018-10-27 18:26:05 +0530

    enum changes in APP

--- pssmonitoring-app/src/enums/triggerType.enum.ts
+++ pssmonitoring-app/src/enums/triggerType.enum.ts
@@ -1,9 +1,9 @@
 export enum TriggerType {
     SHUTDOWN = 0,
     RESTART = 1,
-    LOCK = 3,
-    SIGNOUT = 4,
-    SHOW_MESSAGE = 5,
-    TAKEPICTURE = 6,
-    SCREENSHOT = 7
+    LOCK = 2,
+    SIGNOUT = 3,
+    SHOW_MESSAGE = 4,
+    TAKEPICTURE = 5,
+    SCREENSHOT = 6
 }
